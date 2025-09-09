7269233cbf3dfda551d84c133d9e3580be299764
ManishaChauhan2911 <chauhanmani265@gmail.com>
2025-09-09 11:28:48 +0530

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,42 @@
+import { useState } from "react";
+import {LOGO_URL}  from "../utils/constants";
+
+const Header = () => {
+
+    const [btnNameReact, setBtnNameReact] = useState("login");
+
+    return (
+        <div className="header">
+            <div className="logo-container">
+                <img 
+                  className = "logo"
+                  src={LOGO_URL}
+                />
+            </div>
+
+            <div className="nav-items">
+                <ul>
+                    <li>Home</li>
+                    <li>About Us</li>
+                    <li>Contact US</li>
+                    <li>Cart</li>
+
+                   <button 
+                      className="login" 
+                      onClick={() => {
+                        btnNameReact === "login" ? setBtnNameReact("Logout") : setBtnNameReact("login");
+                      }}
+                    >
+                    
+                    {btnNameReact}
+
+                   </button>
+                  
+                </ul>
+            </div>
+
+        </div>
+    );
+};
+
+export default Header;
---
+++ src/utils/constants.js
@@ -0,0 +1,6 @@
+export const CDN_URL =
+      "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_208,h_208,c_fit/";
+
+export const LOGO_URL =
+       "https://img.freepik.com/premium-vector/burger-logo-design-vector-fast-food-restaurant-cafe-symbol_18099-2286.jpg";
+
